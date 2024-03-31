5d09d9062014f645bd26e7e0a6f87e89492b8747
Ayush Prajapati <ayush.prajapati07@gmail.com>
2024-03-31 13:52:00 +0545

    Project Setup

---
+++ config/settings/env.example.py
@@ -0,0 +1,21 @@
+from .base import *
+
+SECRET_KEY = ''
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': '',
+        'NAME': '',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = 'static/'
